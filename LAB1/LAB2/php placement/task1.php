068388861ae431fd3721661e154c27e2fb789404
Amena Akter <99234497+012amena@users.noreply.github.com>
2023-03-10 16:39:47 +0600

    Delete LAB1 directory

--- LAB1/LAB2/php placement/task1.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function r_area( $var1 , $var2){
-    $area = 2*($var1+$var2);
-    return $area;
-}
-print(r_area( 10 ,20));
-?>
--- LAB1/LAB2/php placement/task2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function vat ( $amount ){
-    $cal_vat = ($amount* 15 )/100;
-    return $cal_vat;
-}
-print(vat(200));
-?>
